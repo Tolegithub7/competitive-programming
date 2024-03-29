94e2ea9e9127432aa0758711fb28062974f7ecfa
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-03-29 22:22:03 +0300

    Time: 406 ms (55.22%) | Memory: 29.4 MB (94.62%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,28 +1,14 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        # skill.sort()
-        # chemistry = 0
-        # for i in range(0, len(skill)//2):
-        #     chemistry += (skill[i]*skill[len(skill)-1-i])
-        # if len(skill)<=2:
-        #     return skill[0]*skill[1]
-        # if len(skill) % 2 == 0 and skill[i] == skill[len(skill)-1-i] and len(skill)>2:
-        #     return chemistry
-        # else:
-        #     return -1
-        
         skill.sort()
-        # lst = []
-        lt, rt = 0, len(skill)-1
+        chem, lt, rt = 0, 0, len(skill)-1
         const = skill[lt] + skill[rt]
-        chem = 0
         while lt<rt:
             const1 = skill[lt] + skill[rt]
             che = skill[lt] * skill[rt]
             if const1 != const:
                 return -1 
                 break
-            # lst.append([skill[lt], skill[rt]])
             lt += 1
             rt -= 1
             chem += che
