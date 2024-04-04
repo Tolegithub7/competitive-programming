fbd415e3b435c39941dacce425f8d48643788805
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-04 15:49:42 +0300

    Time: 122 ms (79.71%) | Memory: 18.2 MB (6.38%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # left = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[left], nums[i] = nums[i], nums[left]
+        #         left += 1
+        # return nums
+
+        holder, seeker, length = 0, 0, len(nums)
+        while seeker<length:
+            if nums[seeker] != 0:
+                nums[holder], nums[seeker] = nums[seeker], nums[holder]
+                holder += 1
+            seeker += 1
+        return nums 
+        
