6e1b770bc981d08f941f0c5e1282f2596bd624ed
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-14 18:38:13 +0300

    Time: 138 ms (47.63%) | Memory: 56.2 MB (95.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -1,11 +1,18 @@
-var isPalindrome = function(x) {
-    var reverse = 0;
-    var copy = x;
-    while (copy > 0) {
-      const digit = copy % 10;
-      reverse = reverse * 10 + digit;
-      copy = ~~(copy / 10);
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0) {
+        return false;
     }
+    let reversed = 0;
+    let temp = x;
 
-    return reverse == x;
+    while(temp!==0){
+        const num = temp%10;
+        reversed = reversed * 10 + num;
+        temp = Math.floor(temp/10);
+    }
+    return reversed === x
 };
