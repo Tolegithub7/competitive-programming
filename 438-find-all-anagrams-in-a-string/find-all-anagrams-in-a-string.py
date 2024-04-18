ad6d41767915ca7df24f2f7a9313c4509f4d6bd7
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-18 23:22:54 +0300

    Time: 281 ms (26.61%) | Memory: 17.7 MB (61.90%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,15 +1,27 @@
 class Solution:
     def findAnagrams(self, s, p):
-        result = []
-        target_counts = collections.Counter(p)
-        window_counts = collections.Counter(s[:len(p) - 1])
-        for i in range(len(p) - 1, len(s)):
-            curr_char = s[i]
-            window_counts[curr_char] += 1
-            if window_counts == target_counts:
-                result.append(i - len(p) + 1)
-            window_counts[s[i - len(p) + 1]] -= 1
-            if window_counts[s[i - len(p) + 1]] == 0:
-                del window_counts[s[i - len(p) + 1]]
-        return result
+        # result = []
+        # target_counts = collections.Counter(p)
+        # win = collections.Counter(s[:len(p) - 1])
+        # for i in range(len(p) - 1, len(s)):
+        #     curr_char = s[i]
+        #     win[curr_char] += 1
+        #     if win == target_counts:
+        #         result.append(i - len(p) + 1)
+        #     win[s[i - len(p) + 1]] -= 1
+        #     if win[s[i - len(p) + 1]] == 0:
+        #         del win[s[i - len(p) + 1]]
+        # return result
         
+        result = []
+        window = Counter(s[:len(p)-1])
+        target = Counter(p)
+        for r in range(len(p)-1, len(s)):
+            current = s[r]
+            window[current] += 1
+            if window == target:
+                result.append(r-len(p)+1)
+            window[s[r-len(p)+1]] -= 1
+            if window[s[r-len(p)+1]] == 0:
+                del window[s[r-len(p)+1]]
+        return result
