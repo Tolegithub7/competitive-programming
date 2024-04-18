5d181f80fd11d6d071f84a6cf9ebcd47488f2127
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-18 22:52:40 +0300

    Time: 47 ms (69.33%) | Memory: 17 MB (64.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = Counter(s)
+        t = Counter(t)
+        if s == t:
+            return True
+        return False
+        # hash_table = {}
+        # if len(s) != len(t):
+        #     return False
+        # for char in s:
+        #     hash_table[char] = hash_table.get(char, 0) + 1
+        #     # if char in hash_table:
+        #     #     hash_table[char] += 1
+        #     # else:
+        #     #     hash_table[char] = 1
+        # for char in t:
+        #     if char in hash_table:
+        #         hash_table[char] -= 1
+        #         if (hash_table[char] == 0):
+        #             del hash_table[char]
+        #     else:
+        #         return False
+        # return True
+
+        # if len(s)!=len(t):return False
+        # for char in set(s):
+        #     if s.count(char) != t.count(char): return False
+        # return True
