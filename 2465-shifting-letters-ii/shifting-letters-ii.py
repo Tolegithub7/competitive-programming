17f6edd08de56df4490ea272c3c887a90b83fd37
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-20 16:22:44 +0300

    Time: 1028 ms (45.17%) | Memory: 41.8 MB (62.78%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        d = [0 for _ in range(n + 1)]
+        for start, end, direction in shifts:
+            dir = 1 if direction else -1
+            d[start] += dir
+            d[end + 1] -= dir
+        
+        res = []
+        for i in range(n):
+            if i != 0: 
+                d[i] += d[i - 1]
+            new_chr_ascii = (ord(s[i]) - ord("a") + d[i]) % 26 + ord("a")
+            res.append(chr(new_chr_ascii))
+        return "".join(res)
+        
