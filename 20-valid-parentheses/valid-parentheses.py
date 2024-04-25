070ac224e723e873fbd122b1340f566583d3e07a
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-25 15:20:15 +0300

    Time: 34 ms (67.57%) | Memory: 16.6 MB (22.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,27 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # stack = []
-        # for char in s:
-        #     if char in ')]}':
-        #         if char == ')' and stack and stack[-1] == '(':
-        #             stack.pop()
-        #         elif char == ']' and stack and stack[-1] == '[':
-        #             stack.pop()
-        #         elif char == '}' and stack and stack[-1] == '}':
-        #             stack.pop()
-        #     else:
-        #         stack.append(char)
-        # return  not stack
-
         stack = []
-        my_dict = {'(':')', '[':']', '{':'}'}
-        for i in range(len(s)):
-            if s[i] in my_dict.keys():
-                stack.append(s[i])
+        if len(s)%2 != 0:
+            return False
+        for char in s:
+
+            if char in ')]}':
+                if char == ')' and stack and stack[-1] == '(':
+                    stack.pop()
+                elif char == ']' and stack and stack[-1] == '[':
+                    stack.pop()
+                elif char == '}' and stack and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
             else:
-                if not stack:
-                    return False
-                a = stack.pop()
-                if s[i] != my_dict[a]:
-                    return False
-        return not stack
+                stack.append(char)
+        return  not stack
+
+        # stack = []
+        # my_dict = {'(':')', '[':']', '{':'}'}
+        # for i in range(len(s)):
+        #     if s[i] in my_dict.keys():
+        #         stack.append(s[i])
+        #     else:
+        #         if not stack:
+        #             return False
+        #         a = stack.pop()
+        #         if s[i] != my_dict[a]:
+        #             return False
+        # return not stack
