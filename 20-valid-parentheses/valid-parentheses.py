c05615c4d4958d6b25ca985157cbf3a3c482235c
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-25 13:32:48 +0300

    Time: 37 ms (46.66%) | Memory: 16.8 MB (5.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # stack = []
+        # for char in s:
+        #     if char in ')]}':
+        #         if char == ')' and stack and stack[-1] == '(':
+        #             stack.pop()
+        #         elif char == ']' and stack and stack[-1] == '[':
+        #             stack.pop()
+        #         elif char == '}' and stack and stack[-1] == '}':
+        #             stack.pop()
+        #     else:
+        #         stack.append(char)
+        # return  not stack
+
+        stack = []
+        my_dict = {'(':')', '[':']', '{':'}'}
+        for i in range(len(s)):
+            if s[i] in my_dict.keys():
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                a = stack.pop()
+                if s[i] != my_dict[a]:
+                    return False
+        return not stack
