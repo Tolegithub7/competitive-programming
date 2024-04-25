349f14c8b9be823ad27c1f7e8afd7d158958c40d
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-04-26 00:30:49 +0300

    Time: 36 ms (65.05%) | Memory: 16.5 MB (85.62%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,36 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        cur = ""
+        for char in path + '/':
+            if char == '/':
+                if cur == '..':
+                    if stack: stack.pop()
+                elif cur != '' and cur != '.': stack.append(cur)
+                cur = ''
+            else: 
+                cur += char 
+        return '/'+"/".join(stack)       
+        # stack = []
+        # cur = ""
+        # for c in path + "/":
+        #     if c == "/":
+        #         if cur == "..":
+        #             if stack:
+        #                 stack.pop()
+        #         elif cur != "" and cur != ".":
+        #             stack.append(cur)
+        #         cur = ""
+        #     else :
+        #         cur += c
+        # return "/" + "/".join(stack)
+        
+        # path_arr = path.split('/')
+        # new_path = []
+        # for folder in path_arr:
+        #     if folder == ".." and len(new_path) > 0:
+        #         new_path.pop(-1)
+        #     elif folder != "." and folder != "" and folder != "..":
+        #         new_path.append(folder)
+        # return "/" + "/".join(new_path)
+        
