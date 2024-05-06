cf6bc6bd7212b9d20ea33b2334df3509728a3a5b
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-06 20:29:03 +0300

    Time: 387 ms (89.86%) | Memory: 51.4 MB (62.56%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -1,33 +1,25 @@
 class Solution:
-    def reverse_list(self, head):
-        prev, current = None, head
-
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        
-        head = prev
-        
-        return head
-
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        head = self.reverse_list(head)
-
-        local_max = 0
-        prev, current = None, head
-
-        while current:
-            local_max = max(local_max, current.val)
-
-            if current.val < local_max:
-                if prev:
-                    prev.next = current.next
-                else:
-                    head = current.next
-                current = current.next
-            else:
-                prev, current = current, current.next
+        #first reverse the list
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
         
-        return self.reverse_list(head)
+        curr = prev
+        while curr:
+            while curr.next and curr.val>curr.next.val:
+                curr.next = curr.next.next
+            curr=curr.next
+
+        curr = prev
+        prev = None
+        while curr:
+            next = curr.next
+            curr.next=prev
+            prev =curr
+            curr = next
+        return prev
