a1f35f37066dca79797e00e2654951048cbdefd7
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-10 09:24:20 +0300

    Time: 908 ms (50.17%) | Memory: 84.9 MB (30.17%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,17 +1,29 @@
 from typing import List
 from heapq import heappush, heappop
-
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        n = len(arr)
-        heap = []
-        for i in range(n):
-            for j in range(i + 1, n):
-                fraction = (arr[i], arr[j])
-                heappush(heap, (arr[i] / arr[j], fraction))
-        for _ in range(k):
-            fra = heappop(heap)[1]
-        return list(fra)
+        heap=[]
+        for i in range(len(arr)):
+            for j in range(len(arr)-1,i,-1):
+                if len(heap)<k:
+                    heapq.heappush(heap,[-(arr[i]/arr[j]),arr[i],arr[j]])
+                elif (arr[i]/arr[j])<-heap[0][0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap,[-(arr[i]/arr[j]),arr[i],arr[j]])
+                else:
+                    break
+        return [heap[0][1],heap[0][2]]
+# class Solution:
+#     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+#         n = len(arr)
+#         heap = []
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 fraction = (arr[i], arr[j])
+#                 heappush(heap, (arr[i] / arr[j], fraction))
+#         for _ in range(k):
+#             fra = heappop(heap)[1]
+#         return list(fra)
 # class Solution:
 #     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
 #         n = len(arr)
