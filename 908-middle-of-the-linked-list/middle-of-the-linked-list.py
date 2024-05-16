b344ff916535e69d50d5fa97b3454de7436f42b1
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-16 20:45:46 +0300

    Time: 35 ms (53.88%) | Memory: 16.7 MB (31.69%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -10,6 +10,7 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+        print(slow)
         return slow
 
         # array_new = []
