8723c62805c62126d6ff6ae0550d2123cb7c2a2b
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-22 23:29:59 +0300

    Time: 41 ms (15.48%) | Memory: 16.4 MB (98.64%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,25 +6,36 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # ans = []
-        # def traverse(node):
-        #     if not node:
-        #         return
-        #     ans.append(node.val)
-        #     traverse(node.left)
-        #     traverse(node.right)
-        #     return
-        # traverse(root)
-        # return ans
+        ans = []
+        def preorder(node):
+            if not node:
+                return
+            ans.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+            return
+        preorder(root)
+        return ans
+  
+        # # ans = []
+        # # def traverse(node):
+        # #     if not node:
+        # #         return
+        # #     ans.append(node.val)
+        # #     traverse(node.left)
+        # #     traverse(node.right)
+        # #     return
+        # # traverse(root)
+        # # return ans
 
-        #Iteration
-        cur, stack = root, []
-        res = []
-        while cur or stack:
-            if cur:
-                res.append(cur.val)
-                stack.append(cur.right)
-                cur = cur.left
-            else:
-                cur = stack.pop()
-        return res
+        # #Iteration
+        # cur, stack = root, []
+        # res = []
+        # while cur or stack:
+        #     if cur:
+        #         res.append(cur.val)
+        #         stack.append(cur.right)
+        #         cur = cur.left
+        #     else:
+        #         cur = stack.pop()
+        # return res
