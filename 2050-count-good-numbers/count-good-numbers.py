8b23b44a433a70c022b41f739744e79fd6b71f99
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-23 00:57:32 +0300

    Time: 26 ms (98.10%) | Memory: 16.5 MB (97.38%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,11 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.MOD = 10**9 + 7
-    @cache
+    # def __init__(self):
+    #     self.MOD = 10**9 + 7
+    # @cache
     def countGoodNumbers(self, n: int) -> int:
-        if n == 1: return 5
-        if n == 2: return 20
-        if (n // 2) % 2 == 0:
-            return (self.countGoodNumbers(n//2) * self.countGoodNumbers((n+1)//2)) % self.MOD
-        else:
-            return (self.countGoodNumbers(n//2+1) * self.countGoodNumbers((n+1)//2-1)) % self.MOD
+    #     if n == 1: return 5
+    #     if n == 2: return 20
+    #     if (n // 2) % 2 == 0:
+    #         return (self.countGoodNumbers(n//2) * self.countGoodNumbers((n+1)//2)) % self.MOD
+    #     else:
+    #         return (self.countGoodNumbers(n//2+1) * self.countGoodNumbers((n+1)//2-1)) % self.MOD
+
+        MOD = 10**9 + 7
+        even_places = (n + 1) // 2
+        odd_places = n // 2
+
+        ans = (pow(5, even_places, MOD) * pow(4, odd_places, MOD)) % MOD
+        return ans
