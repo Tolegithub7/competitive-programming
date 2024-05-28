44d1d1f0e9c703edb344adaec0e1bc6e2b0db38b
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-05-28 10:42:32 +0300

    Create  Code Testcase Test Result Test Result 225. Implement Stack using Queues.py

---
+++ 225-implement-stack-using-queues/Code Testcase Test Result Test Result 225. Implement Stack using Queues.py
@@ -0,0 +1,26 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+         return self.stack[-1]
+
+    def empty(self) -> bool:
+        if len(self.stack) == 0:
+            return True
+        return False
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
