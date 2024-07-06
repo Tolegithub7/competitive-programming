2f2bf9cda4e7f6631564f975026d17ba6c6ce7de
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-07-06 22:46:31 +0300

    Time: 28 ms (91.13%) | Memory: 16.5 MB (77.09%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.py
+++ 2645-pass-the-pillow/pass-the-pillow.py
@@ -1,6 +1,12 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        idx = 1
-        for i in range(time):
-            idx += 1 - 2 * (i % (2 * (n - 1)) >= n - 1)
-        return idx
+        current_pillow_position = 1
+        current_time = 0
+        direction = 1
+        while current_time < time:
+            if 0 < current_pillow_position + direction <= n:
+                current_pillow_position += direction
+                current_time += 1
+            else:
+                direction *= -1
+        return current_pillow_position
