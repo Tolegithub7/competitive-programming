459f49f0faec0c1a045984e2b6ccc22bc20199b0
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-07-20 23:59:27 +0300

    Time: 351 ms (99.31%) | Memory: 21.7 MB (79.86%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.py
@@ -1,30 +1,24 @@
 class Solution:
-    def restoreMatrix(self, rowSum: list[int], colSum: list[int]) -> list[list[int]]:
-        rows: int = len(rowSum)
-        cols: int = len(colSum)
-        cur_row: int = 0
-        cur_col: int = 0
-        res: list[list[int]] = [[0 for _ in range(cols)] for _ in range(rows)]
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        """
+        
+        """
+        n = len(rowSum)
+        m = len(colSum)
+        
+        mat = [[0]*m for i in range(n)]
+        i = 0
+        j = 0
+        while i < n and j < m:
+            mat[i][j] = min(rowSum[i], colSum[j])
+            if rowSum[i] == colSum[j]:
+                i += 1
+                j += 1
+            elif rowSum[i] > colSum[j]:
+                rowSum[i] -= colSum[j]
+                j += 1
+            else:
+                colSum[j] -= rowSum[i]
+                i += 1
 
-        while cur_row < rows or cur_col < cols:
-            if cur_row >= rows:
-                res[rows - 1][cur_col] = colSum[cur_col]
-                cur_col += 1
-                continue
-            elif cur_col >= cols:
-                res[cur_row][cols - 1] = rowSum[cur_row]
-                cur_row += 1
-                continue
-
-            value_to_put: int = min(rowSum[cur_row], colSum[cur_col])
-            rowSum[cur_row] -= value_to_put
-            colSum[cur_col] -= value_to_put
-            res[cur_row][cur_col] = value_to_put
-
-            # I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
-            if rowSum[cur_row] == 0:
-                cur_row += 1
-            if colSum[cur_col] == 0:
-                cur_col += 1
-
-        return res
+        return mat
