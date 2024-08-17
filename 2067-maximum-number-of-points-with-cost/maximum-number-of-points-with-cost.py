bfa93be2f30816637bbdf5de66a87af550717fd1
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-08-18 00:05:33 +0300

    Time: 1438 ms (95.83%) | Memory: 50.9 MB (51.85%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -1,15 +1,25 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        lenRow, lenCol = len(points), len(points[0])
-        leftMax, rightMax = [0] * lenCol, [0] * lenCol
-        currRow = points[0]
-        for row in points[1:]:
-            leftMax[0], rightMax[-1] = currRow[0], currRow[-1]
-            for i in range(1, lenCol):
-                leftMax[i] = max(leftMax[i-1]-1, currRow[i])
-            for i in range(lenCol-2, -1, -1):
-                rightMax[i] = max(rightMax[i+1]-1, currRow[i])
-            for i in range(lenCol):
-                currRow[i] = max(leftMax[i], rightMax[i]) + row[i]
+#         lenRow, lenCol = len(points), len(points[0])
+#         leftMax, rightMax = [0] * lenCol, [0] * lenCol
+#         currRow = points[0]
+#         for row in points[1:]:
+#             leftMax[0], rightMax[-1] = currRow[0], currRow[-1]
+#             for i in range(1, lenCol):
+#                 leftMax[i] = max(leftMax[i-1]-1, currRow[i])
+#             for i in range(lenCol-2, -1, -1):
+#                 rightMax[i] = max(rightMax[i+1]-1, currRow[i])
+#             for i in range(lenCol):
+#                 currRow[i] = max(leftMax[i], rightMax[i]) + row[i]
             
-        return max(currRow)
+#         return max(currRow)
+
+        m, n = len(points), len(points[0])
+        for i in range(m - 1):
+            for j in range(1, n):
+                points[i][j] = max(points[i][j], points[i][j - 1] - 1)
+            for j in range(n - 2, -1, -1):
+                points[i][j] = max(points[i][j], points[i][j + 1] - 1)
+            for j in range(n):
+                points[i + 1][j] += points[i][j]   
+        return max(points[m - 1])
