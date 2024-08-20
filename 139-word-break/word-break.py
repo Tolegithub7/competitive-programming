b1e16dc1bdb877039c9ea7aca8a02bb76a37f288
Tole <132619843+Tolegithub7@users.noreply.github.com>
2024-08-20 15:41:40 +0300

    Time: 37 ms (75.36%) | Memory: 16.7 MB (22.04%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        @cache
+        def wordBrk(i):
+            if i == n:
+                return True
+            for word in wordDict:
+                j = i
+                useWord = True
+                for ch in word:
+                    if j >= n or s[j] != ch:
+                        useWord = False
+                        break
+                    j += 1
+                if useWord and wordBrk(j):
+                    return True
+            return False
+        return wordBrk(0)
