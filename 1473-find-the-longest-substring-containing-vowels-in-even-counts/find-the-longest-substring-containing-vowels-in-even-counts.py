b350a85673b061a195172a3278870b57d0c4dbc7
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-09-15 23:53:00 +0300

    Time: 324 ms (33.42%) | Memory: 22.1 MB (79.45%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,27 +1,9 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        mapy = [-2] * 32
-        mapy[0] = -1
-
-        max_len = 0
-        mask = 0
-
-        for i, char in enumerate(s):
-            if char == 'a':
-                mask ^= 1
-            elif char == 'e':
-                mask ^= 2
-            elif char == 'i':
-                mask ^= 4
-            elif char == 'o':
-                mask ^= 8
-            elif char == 'u':
-                mask ^= 16
-
-            prev = mapy[mask]
-            if prev == -2:
-                mapy[mask] = i
-            else:
-                max_len = max(max_len, i - prev)
-
-        return max_len
+        dic_mask = {'a' : 1, 'e' : 2, 'i' : 4, 'o' : 8, 'u' : 16}
+        d = {}
+        ans, pref, d[0] = 0, 0, -1
+        for i, ch in enumerate(s):
+            pref ^= dic_mask.get(ch, 0)
+            ans = max(i - d.setdefault(pref, i), ans)
+        return ans
