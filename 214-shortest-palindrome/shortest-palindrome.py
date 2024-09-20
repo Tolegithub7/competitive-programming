7c4ba7d1d8ea6fade04ec100dc5f1d592ede4235
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-09-20 23:43:34 +0300

    Time: 60 ms (75.26%) | Memory: 17.2 MB (33.45%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,23 +1,17 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-
-        rev = ''
-        n = len(s)
+        rev,n = '',len(s)
         rev1 = ''
         i = 0
-
         for j in range(n):
             rev += s[n-j-1]
 
         for j in range(n-1,-1,-1):
             if s[i] == s[j]:
                 i += 1
-
         if i == n:
             return s
-
         string = s[i:n]
-
         for j in range(len(string)):
             rev1 += string[len(string)-j-1]
 
