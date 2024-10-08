69bee553fadade26eb1407773f3bccc1a1d83f64
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-10-08 22:20:01 +0300

    Time: 164 ms (94.29%) | Memory: 27.8 MB (98.61%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,10 +1,20 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
+#         stack_size = 0
+#         for ch in s:
+#             if ch == "[":
+#                 stack_size += 1
+#             else:
+#                 if stack_size > 0:
+#                     stack_size -= 1
+#         return (stack_size + 1) // 2
         stack_size = 0
+
         for ch in s:
             if ch == "[":
                 stack_size += 1
             else:
                 if stack_size > 0:
                     stack_size -= 1
+
         return (stack_size + 1) // 2
