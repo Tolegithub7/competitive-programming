71e35dd88055f0f075c6869ccb3739c4b9b55965
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-10-18 00:22:46 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (10.93%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,10 +1,27 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        lists = list(str(num))
-        last = {int(d): i for i, d in enumerate(lists)}
-        for i, digit in enumerate(lists):
-            for d in range(9, int(digit), -1):
-                if last.get(d, -1) > i:
-                    lists[i], lists[last[d]] = lists[last[d]], lists[i]
-                    return int(''.join(lists))
-        return num
+        # lists = list(str(num))
+        # last = {int(d): i for i, d in enumerate(lists)}
+        # for i, digit in enumerate(lists):
+        #     for d in range(9, int(digit), -1):
+        #         if last.get(d, -1) > i:
+        #             lists[i], lists[last[d]] = lists[last[d]], lists[i]
+        #             return int(''.join(lists))
+        # return num
+
+        ans = list(str(num))
+        n = len(ans)
+        highest = -1
+        swapL = -1
+        swapR = -1
+        for i in range(n-1,-1,-1):
+            digit = ans[i]
+            if highest == -1 or digit > ans[highest]:
+                highest = i
+            elif digit < ans[highest]:
+                swapL = i
+                swapR = highest
+        if swapR != -1 and swapL != -1:
+            ans[swapR],ans[swapL] = ans[swapL],ans[swapR]
+
+        return int(''.join(ans))
