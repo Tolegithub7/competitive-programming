53fcaa7c3e056341a9e40b7533ab404c7b67f62c
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-10-18 00:23:07 +0300

    Time: 2 ms (100.00%) | Memory: 16.5 MB (42.71%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -1,27 +1,16 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        # lists = list(str(num))
-        # last = {int(d): i for i, d in enumerate(lists)}
-        # for i, digit in enumerate(lists):
-        #     for d in range(9, int(digit), -1):
-        #         if last.get(d, -1) > i:
-        #             lists[i], lists[last[d]] = lists[last[d]], lists[i]
-        #             return int(''.join(lists))
-        # return num
+        num = list(str(num))
+        n = len(num)
 
-        ans = list(str(num))
-        n = len(ans)
-        highest = -1
-        swapL = -1
-        swapR = -1
-        for i in range(n-1,-1,-1):
-            digit = ans[i]
-            if highest == -1 or digit > ans[highest]:
-                highest = i
-            elif digit < ans[highest]:
-                swapL = i
-                swapR = highest
-        if swapR != -1 and swapL != -1:
-            ans[swapR],ans[swapL] = ans[swapL],ans[swapR]
-
-        return int(''.join(ans))
+        for i in range(n):
+            maxindex = i
+            for j in range(n-1, i, -1):
+                if int(num[j]) > int(num[maxindex]):
+                    maxindex = j
+                
+            if maxindex != i:
+                num[i], num[maxindex] = num[maxindex], num[i]
+                break
+        
+        return int("".join(num))
